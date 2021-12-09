8501cc8af0a99e5a10ab0f777e603045542d40fe
Mochamad Revanza Kurniawan <mrevanzak@gmail.com>
2021-12-09 17:47:22 +0700

    Fix About Frame

--- src/AboutFrame.java
+++ src/AboutFrame.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -18,7 +18,7 @@ public class AboutFrame {
         frame = new JFrame("About");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         backButton = new JButton("Close");
-        about = new JPanel();
+        about = new JPanel(new GridBagLayout());
         backButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 frame.setVisible(false);
@@ -27,6 +27,7 @@ public class AboutFrame {
         frame.getContentPane().setBackground(Color.WHITE);
         about.setBackground(Color.WHITE);
         frame.setResizable(true);
+        frame.setLayout(new GridBagLayout());
 
         JLabel description = new JLabel("<html><h2>Built by</h2>"+
         "    <ol>"+
@@ -36,35 +37,35 @@ public class AboutFrame {
         "        <li>Mochamad Revanza Kurniawan - 5025201145</li>"+
         "    </ol>"+
         "    <h2>Description</h2>"+
-        "    <p>The Haunted Maze adalah game puzzle labirin klasik yang bertipe single player dan bisa dimainkan oleh segala usia mulai dari anak-anak, remaja hingga dewasa. "+
+        "    <p style=\"text-indent: 30px;\">The Haunted Maze adalah game puzzle labirin klasik yang bertipe single player dan bisa dimainkan oleh segala usia mulai dari anak-anak, remaja hingga dewasa. "+
         "        Pemain harus menentukan jalan yang tepat untuk keluar dari labirin yang berliku dan banyak jalan buntu juga hantu yang berkeliaran. "+
         "        Untuk membuka pintu keluar labirin, pemain membutuhkan sebuah item kunci yang terdapat di suatu tempat dalam labirin. "+
         "        Terdapat juga beberapa item yang membantu player seperti pedang untuk mengalahkan hantu dan es untuk membekukan hantu beberapa saat. "+
         "        Aplikasi ini adalah game edukasi yang diinovasikan dengan menambah unsur petualangan (adventure) di dalamnya sehingga tidak membosankan.</p>"+
         "        "+
-        "    <p>Permainan ini dimainkan di PC dengan menggunakan control button keyboard yakni 'W', 'A', 'S', 'D' atau Arrow Button untuk menjalankan player. "+
+        "    <p style=\"text-indent: 30px;\">Permainan ini dimainkan di PC dengan menggunakan control button keyboard yakni 'W', 'A', 'S', 'D' atau Arrow Button untuk menjalankan player. "+
         "        Game ini dilengkapi dengan level up system yang akan menjadi daya tarik bagi pemain untuk mengasah logika dan keterampilan dalam menyelesaikan variasi tantangan.</p>");
-        
-        // GridBagConstraints c = new GridBagConstraints();
+            
+        GridBagConstraints c = new GridBagConstraints();
 
-        // c.fill = GridBagConstraints.HORIZONTAL;
-        // c.gridx = 0;
-        // c.gridy = 0;
-        about.add(description, BorderLayout.PAGE_START);
+        c.fill = GridBagConstraints.CENTER;
+        c.ipady = 90;
+        c.ipadx = 220;
+        about.add(description, c);
 
-        // c.gridy= 2;
-		// c.anchor = GridBagConstraints.PAGE_END;
-		// c.insets = new Insets(0,0,0,0);
-		// this.backButton.setEnabled(true);
-		// about.add(backButton, BorderLayout.PAGE_END);
+
+        c.ipady = 0;
+        c.ipadx = 0;
+        c.gridy = 1;
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(0,0,0,0);
+		this.backButton.setEnabled(true);
+        about.add(backButton, c);
 		
-		// c.gridx= 0;
 		frame.add(about);
-
-		// c.gridy = 1;
 		frame.pack();
+        frame.setSize(500, 650);
 		frame.setVisible(false);
-        frame.setSize(300, 300);
     }
 
     public void setVisible(boolean b) {
